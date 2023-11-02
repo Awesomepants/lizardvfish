236798b19a568d427cda45aa0ce8a5f44f837848
Benjamin <legoawesomepants@gmail.com>
2023-11-02 18:05:28 -0500

    Sticking to walls, more tweaking the physics

--- script.js
+++ script.js
@@ -11,11 +11,12 @@ class Example extends Phaser.Scene
         this.matter.world.setBounds();
 
         //  Our two bodies which will be connected by a constraint (aka a Joint or a Spring)
-
-        this.lizardHead = this.matter.add.image(420, 100, 'lizardHead', null, { shape: 'circle', friction: 0.1, restitution: 0.6 });
-        this.lizardTail = this.matter.add.image(400, 200, 'lizardTail', null, { shape: 'circle', friction: 0.1, restitution: 0.6 });
+        
+        this.lizardHead = this.matter.add.image(420, 100, 'lizardHead', null, { shape: 'circle', friction: 0, restitution: 0.6 });
+        this.lizardTail = this.matter.add.image(400, 200, 'lizardTail', null, { shape: 'circle', friction: 0, restitution: 0.6 });
         this.lizardHead.setFrictionAir(0.1);
         this.lizardTail.setFrictionAir(0.2);
+        this.lizardHead.body.label = "lizardHead";
         //  You can create a constraint between the two bodies using a Factory function.
         //  The value 100 is the resting length and 0.2 is the stiffness of the constraint.
 
@@ -24,15 +25,38 @@ class Example extends Phaser.Scene
 
         this.cursors = this.input.keyboard.createCursorKeys();
         this.moveLizard = (x,y) => {
-            const force = 0.005;
-            const tailForce = 0;
-            
+            let force = 0.002;
+            let tailForce = 0;
+            if(this.lizardHead.sticking){
+                force = 0.009;
+            }
             this.lizardHead.applyForce(new Phaser.Math.Vector2(x*force,y*force));
             this.lizardTail.applyForce(new Phaser.Math.Vector2(x*tailForce,y*tailForce));
+            let lizardVelocity = this.lizardHead.getVelocity();
+            let lizardAngle = Math.atan2(lizardVelocity.y, lizardVelocity.x);
+            this.lizardHead.setAngle(lizardAngle);
             
-
-            console.log(this.lizardHead.rotation)
         }
+        
+        this.matter.world.on("collisionend",(e,o1,o2) => {
+            if(o1.label === "lizardHead" || o2.label === "lizardHead"){
+                console.log("lizardHead lifted");
+                this.lizardHead.sticking = false;
+            }
+        })
+        this.matter.world.on("collisionactive",(e,o1,o2) => {
+            if(o1.label === "lizardHead" || o2.label === "lizardHead"){
+                console.log("lizardHead is touching");
+                const collisionNormal = e.pairs[0].collision.normal;
+                const stickingAmount = 0.5;
+                const stickingVector = new Phaser.Math.Vector2(collisionNormal.x * stickingAmount, collisionNormal.y * stickingAmount);
+                this.lizardHead.applyForce(stickingVector);
+                this.lizardTail.applyForce(stickingVector);
+                console.log(stickingVector);
+                this.lizardTail
+                this.lizardHead.sticking = true;
+            }
+        })
 
     }
     update()
@@ -61,7 +85,7 @@ const config = {
         default: 'matter' ,
         matter:{
             gravity:{
-                y: 1
+                y: 0.5
             }
         }  
     },
