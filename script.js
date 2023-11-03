d8dcbfddbfe49a5b75ffc993e6b0c3bb9343cad7
Benjamin <legoawesomepants@gmail.com>
2023-11-03 17:36:18 -0500

    changed name of folder and stuffs

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ class Example extends Phaser.Scene
 {
     preload ()
     {
-        this.load.image('lizardHead', 'assets/lizardHead.png');
-        this.load.image('lizardTail', 'assets/lizardTail.png');
-        this.load.aseprite('bodySegment','assets/sprites/bodysegment.png','/assets/sprites/bodysegment.json');
+        this.load.image('lizardHead', 'gameassets/lizardHead.png');
+        this.load.image('lizardTail', 'gameassets/lizardTail.png');
+        this.load.aseprite('bodySegment','gameassets/sprites/bodysegment.png','/assets/sprites/bodysegment.json');
     }
 
     create ()
