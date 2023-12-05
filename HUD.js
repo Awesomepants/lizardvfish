9934fe28a47a105114e9a6a4fb86a05c4d79c282
Benjamin <legoawesomepants@gmail.com>
2023-12-05 15:12:41 -0600

    Poki improvements

--- HUD.js
+++ HUD.js
@@ -1,8 +1,11 @@
 const createHUD = (scene, lizard) => {
-    const healthText = scene.add.text(10,10,"Health").setScrollFactor(0,0);
-    const oxygenText = scene.add.text(10,30, "Oxygen").setScrollFactor(0,0);
+    const healthText = scene.add.text(50,20,"Health", {fontSize:"30px"}).setScrollFactor(0,0);
+    const oxygenText = scene.add.text(50,60, "Oxygen", {fontSize:"30px"}).setScrollFactor(0,0);
+    scene.add.sprite(30,30,"icons",3).setScrollFactor(0,0);
+    scene.add.sprite(30,70,"icons",2).setScrollFactor(0,0);
     scene.matter.world.on("afterupdate",()=>{
-        healthText.text = `Health: ${lizard.health}`;
-        oxygenText.text = `Oxygen: ${Math.floor(lizard.oxygen)}`;
+        healthText.text = `${lizard.health}`;
+        oxygenText.text = `${Math.floor(lizard.oxygen)}`;
     })
+
 }
